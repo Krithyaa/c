9618e36ef5af9647f9983b48fdc43b2637af1454
Krithyaa <143446267+Krithyaa@users.noreply.github.com>
2023-10-31 23:24:29 +0530

    firstocc.cpp

--- firstocc.cpp
+++ firstocc.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 int main()
 {
 	int i,n,k,j;
@@ -9,17 +8,17 @@ int main()
 	{
 		scanf("%d",&a[i]);		
 	}
-	
+	int c=0;
+
 	for(i=0;i<n;i++){
+	
 		for(j=i+1;j<n;j++){
 			if (a[i]==a[j])
-			printf("Duplicate element is %d",a[j]);
-			
-			
-			}
-
+			k=a[i];
+			c++;}
+	if (c>0){
 	
-	}
-
+	printf("%d",k);
+	break;}
 }
-
+}
