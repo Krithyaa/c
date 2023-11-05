ad0cf86e19c9b50578d43ba08647920b4f40e890
Krithyaa <143446267+Krithyaa@users.noreply.github.com>
2023-11-05 17:04:23 +0530

    pat15.cpp

---
+++ pat15.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int i,n,j,k=1;
+	printf("enter n value");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		for(j=0;j<i+1;j++) {
+		 
+			printf("%d ",k);
+			k++;}
+			printf("\n");}
+	
+  
+}
