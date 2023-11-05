8aca08321a499ea2bd34211eb0232e39c807dbc2
Krithyaa <143446267+Krithyaa@users.noreply.github.com>
2023-11-05 17:07:58 +0530

    Add files via upload

---
+++ pat18.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int i,n,j,k=1;
+	printf("enter n value");
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		for(j=0;j<n-i;j++) {
+		
+			printf("%d ",k);
+			k++;}
+			printf("\n");
+}
+  
+}
