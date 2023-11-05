e3677e3d5a3374bfb03c908dec37b16bbd1f9726
Krithyaa <143446267+Krithyaa@users.noreply.github.com>
2023-11-05 16:59:01 +0530

    pat19.cpp

--- pat19.cpp
+++ pat19.cpp
@@ -4,11 +4,11 @@ int main()
 	int i,n,j,s;
 	printf("enter n value");
 	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
-	{   for(s=1;s<n-i;s++)
+	for(int i=0;i<n;i++)
+	{   for(s=0;s<n-i-1;s++)
 	        printf(" ");
-	    {   for(j=0;j<i;j++)
-                printf("%d ",i);
+	    {   for(j=0;j<=i;j++)
+                printf("%d ",i+1);
 				printf("\n");
 			}
 	}
