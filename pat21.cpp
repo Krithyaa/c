e36b2bdddef280630d8be8b41cc3008c3a12bd52
Krithyaa <143446267+Krithyaa@users.noreply.github.com>
2023-11-05 17:51:20 +0530

    Update and rename patt8.cpp to pat21.cpp

--- pat21.cpp
+++ pat21.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	int i,n,j;
+	int i,n,j,a=65;
 	printf("enter n value");
 	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
+	for(int i=0;i<n;i++)
 	{
-		for(j=1;j<i+1;j++) 
-			printf("%d",i);
+		for(j=0;j<i+1;j++) {
+		
+			printf("%c",a);}
+			a++;
 			printf("\n");
 	} 
   
